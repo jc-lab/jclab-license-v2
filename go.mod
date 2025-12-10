6bec0563930455e4be21d185aa353f6493fec48f
Joseph Lee <joseph@jc-lab.net>
2025-12-11 00:22:27 +0900

    feat: use 64-bit keyid for timecense

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.20
 
 require google.golang.org/protobuf v1.34.2
 
-require go.mozilla.org/pkcs7 v0.9.0 // indirect
+require go.mozilla.org/pkcs7 v0.9.0
--- license_proto/generator.go
+++ license_proto/generator.go
@@ -1,6 +1,12 @@
+//go:build tools
+
 package license_proto
+
+import (
+    _ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
 
 // prepare
 // go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 
-//go:generate protoc --proto_path=../java/jclab-license-model/src/main/proto/ --go_out=. --go_opt=paths=source_relative jclab-license.proto
+//go:generate protoc --proto_path=../java/jclab-license-model/src/main/proto/ --go_out=. --go_opt=paths=source_relative jclab_license.proto
